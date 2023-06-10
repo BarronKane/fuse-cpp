ebee56b337500b61eebdf3d1b9bd863baef493c6
Lance Wallis <bleudeveloper@gmail.com>
2023-06-10 16:13:28 -0500

    Initial qt window impl.

--- src/gui/private/gui/gui.cpp
+++ src/gui/private/gui/gui.cpp
@@ -1,2 +1,13 @@
 #include "gui/gui.h"
 
+FuseGui::FuseGui(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::FuseGui)
+{
+	ui->setupUi(this);
+}
+
+FuseGui::~FuseGui()
+{
+	delete ui;
+}
--- src/gui/public/gui/gui.h
+++ src/gui/public/gui/gui.h
@@ -1,2 +1,22 @@
 #pragma once
 
+#include <QMainWindow>
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class FuseGui; }
+QT_END_NAMESPACE
+
+class FuseGui : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+
+	FuseGui(QWidget *parent = nullptr);
+	~FuseGui();
+
+private:
+
+	Ui::FuseGui *ui;
+};
